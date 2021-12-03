221e5cb2e46b6b54c28a7c2be4458cd40b9f5ef5
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-12-03 11:46:45 -0500

    made control with the handling of args

---
+++ control.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <strings.h>
+
+int main(int argc, char* argv[]){
+    if (argc > 1){
+        if (strcmp(argv[1], "create") == 0){
+            
+        }else if (strcmp(argv[1], "remove") == 0){
+            
+        }
+    }
+}
