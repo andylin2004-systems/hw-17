6fd6c8c986391aea060f85df33ac37cb1f1ec033
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-12-06 15:25:21 -0500

    better handling of issues

--- control.c
+++ control.c
@@ -41,15 +41,21 @@ int main(int argc, char *argv[])
             printf("%s\n", fileContent);
             close(file);
         }
-        else
+        else if (strcmp(argv[1], "create") == 0)
         {
             semd = semget(SEMKEY, 1, IPC_CREAT | IPC_EXCL | 0640);
             memd = shmget(MEMKEY, sizeof(int), IPC_CREAT | IPC_EXCL | 0640);
-            union semun us;
+            if (semd == -1){
+                printf("%s\n", strerror(semd));
+            }else{
+                union semun us;
             us.val = 1;
             semctl(semd, 0, SETVAL, us);
             file = open("telephone.txt", O_CREAT | O_WRONLY | O_TRUNC, 0644);
             close(file);
+            }
+        }else{
+            printf("Use args 'create' or 'remove' to use this.");
         }
     }
 }
